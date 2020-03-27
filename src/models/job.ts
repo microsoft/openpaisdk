e8cc78013acc1486fadab51250a2d825db175104
Yuqing Yang <yuqyang@microsoft.com>
2020-03-27 10:08:18 +0800

    update headers of source code

--- src/models/job.ts
+++ src/models/job.ts
@@ -1,19 +1,5 @@
-// Copyright (c) Microsoft Corporation
-// All rights reserved.
-//
-// MIT License
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
-// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 /**
  * OpenPAI Job Info.
@@ -32,7 +18,7 @@ export interface IJobInfo {
         /** Job failed due to platform error. */
         platform: number,
         /** Job cannot get required resource to run within timeout. */
-        resource: number
+        resource: number;
     };
     createdTime: number;
     completedTime: number;
@@ -209,11 +195,11 @@ export interface IJobConfig {
                 ports?: {
                     /** Port number for the port label. Only for host network, portLabel string in ^[A-Za-z0-9\-._~]+$ format. */
                     [portLabel: string]: number;
-                }
+                };
             };
 
             commands: string[];
-        }
+        };
     };
 
     /**
@@ -234,8 +220,8 @@ export interface IJobConfig {
                 preCommands: string[];
                 /** Execute after the taskRole's command. */
                 postCommands: string[];
-            }
-        }
+            };
+        };
     }[];
 
     /** Optional, default cluster specific settings. */
