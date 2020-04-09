acff0a3ad9f53612b1a88c5673f3755d35d53e7b
yiyione <yiyi_one@foxmail.com>
2020-04-09 11:32:18 +0800

    add constants and errors

---
+++ src/commom/constants.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * The constants.
+ */
+export const PATH_NOT_FOUND: string = 'Path not found.';
---
+++ src/commom/errors.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * Base PAI error class.
+ */
+export class PAIError extends Error {
+}
