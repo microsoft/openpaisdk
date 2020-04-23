67ae1a6df825680f58a651e2d8786b4da8f8e42f
Yuqing Yang <yuqyang@microsoft.com>
2020-04-23 18:00:47 +0800

    fix typo

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-require('module-alias/register'); // tslint:disable-next-line:no-require-imports no-var-requires
+
+// tslint:disable-next-line:no-require-imports no-var-requires
+require('module-alias/register');
 
 import { registerBuiltinCommands, CliEngine } from './commands';
 import { IResult } from './commands/cliEngine';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-require('module-alias/register'); // tslint:disable-next-line:no-require-imports no-var-requires
+
+// tslint:disable-next-line:no-require-imports no-var-requires
+require('module-alias/register');
 
 import * as PAIV1 from './api/v1';
 import * as PAIV2 from './api/v2';
