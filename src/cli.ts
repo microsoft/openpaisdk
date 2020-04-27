1ba63ae8d21b2f5efb6aa72505ae7276d2b5d37a
Yuqing Yang <yuqyang@microsoft.com>
2020-04-27 17:01:40 +0800

    fix paths mapping  (#32)
    * Revert "resolve paths mapping by module-alias (#31)"
    
    This reverts commit c7a0014665caa2337edb1b835ef601404ba15ee3.
    
    * use transformers to resolve path mapping

--- src/cli.ts
+++ src/cli.ts
@@ -2,9 +2,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-// tslint:disable-next-line:no-require-imports no-var-requires
-require('module-alias/register');
-
 import { registerBuiltinCommands, CliEngine } from './commands';
 import { IResult } from './commands/cliEngine';
 
